21f4ba8f0dc090d31d7d5af92881b82fff5499c1
davidaparicioc4g <97703358+davidaparicioc4g@users.noreply.github.com>
2022-04-24 14:54:32 -0400

    Update streamlit_app.py
    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,6 +23,6 @@ streamlit.dataframe(fruits_to_show)
 
 #New section to dispay fuirvice api response
 import requests
-fruitvice_response = requests.get("https://fruitvice.com/api/fruit/watermelon")
-streamlit.text(fruitvice_response)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
 
